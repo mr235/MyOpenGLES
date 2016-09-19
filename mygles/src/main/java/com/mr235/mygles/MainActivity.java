5d485266fe1ca7967c045db329daf0743cafd42b
Mr235 <cnmr235@hotmail.com>
2016-09-19 18:57:36 +0800

    draw

--- mygles/src/main/java/com/mr235/mygles/MainActivity.java
+++ mygles/src/main/java/com/mr235/mygles/MainActivity.java
@@ -20,41 +20,4 @@ public class MainActivity extends AppCompatActivity {
 
 		setContentView(mGLView);
 	}
-
-	static class MyGLSurfaceView extends GLSurfaceView {
-
-		private final MyGLRenderer mRenderer;
-
-		public MyGLSurfaceView(Context context){
-			super(context);
-
-			// Create an OpenGL ES 2.0 context
-			setEGLContextClientVersion(2);
-
-			mRenderer = new MyGLRenderer();
-
-			// Set the Renderer for drawing on the GLSurfaceView
-			setRenderer(mRenderer);
-
-			// Render the view only when there is a change in the drawing data
-			setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
-		}
-	}
-	static class MyGLRenderer implements GLSurfaceView.Renderer {
-
-		@Override
-		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-			GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-		}
-
-		@Override
-		public void onSurfaceChanged(GL10 gl, int width, int height) {
-			GLES20.glViewport(0, 0, width, height);
-		}
-
-		@Override
-		public void onDrawFrame(GL10 gl) {
-			GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-		}
-	}
 }
